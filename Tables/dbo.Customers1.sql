b87ea580bab2a4c99869dd7f1636a3d6e09d7051
kammarac9 <kammarac9@gmail.com>
2024-12-29 08:28:51 -0800

    commit

---
+++ Tables/dbo.Customers1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customers1] (
+  [CustomerID] [int] IDENTITY,
+  [FirstName] [varchar](100) NOT NULL,
+  [LastName] [varchar](100) NOT NULL,
+  [Email] [varchar](255) NOT NULL,
+  [DateOfBirth] [date] NULL,
+  [DateJoined] [datetime] NULL DEFAULT (getdate()),
+  PRIMARY KEY CLUSTERED ([CustomerID])
+)
+ON [PRIMARY]
+GO
