238b2dcdb4313ccd5cfe0e8fef94ec757546c67d
kammarac9 <kammarac9@gmail.com>
2024-12-29 05:29:32 -0800

    commit

---
+++ Tables/dbo.Orders.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Orders] (
+  [OrderID] [int] IDENTITY,
+  [CustomerID] [int] NULL,
+  [OrderDate] [datetime] NULL DEFAULT (getdate()),
+  [Amount] [decimal](10, 2) NULL,
+  [Status] [varchar](50) NULL,
+  PRIMARY KEY CLUSTERED ([OrderID])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Orders]
+  ADD FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID])
+GO
---
+++ Tables/dbo.tbl1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbl1] (
+  [id] [int] NOT NULL,
+  [NAME] [varchar](35) NULL,
+  PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
